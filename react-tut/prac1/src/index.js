0f9dee5971e23cec6dee8aa7fc84ff7f9384e28c
sonallilive <sonallilive97@gmail.com>
2020-10-06 01:52:02 +0530

    with one more element created

--- react-tut/prac1/src/index.js
+++ react-tut/prac1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <h1>Hello</h1>,
   document.getElementById('root')
 );
+ReactDOM.render(<h3>will work ?</h3>,document.getElementById('will'));
