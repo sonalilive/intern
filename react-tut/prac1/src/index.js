3e926411eaf066c8dc4947c51907f3fc8fb1dd57
sonallilive <sonallilive97@gmail.com>
2020-10-06 12:24:12 +0530

    with variable

--- react-tut/prac1/src/index.js
+++ react-tut/prac1/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const name='sonali';
+const ele=<h1>hello {name}</h1>;
 ReactDOM.render(
-  <h1>Hello</h1>,
+  ele,
   document.getElementById('root')
 );
 ReactDOM.render(<h3>will work ?</h3>,document.getElementById('will'));
