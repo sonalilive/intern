eb5dbcc7ff572ee806f12e69a2bd7c277f2e4b7f
sonallilive <sonallilive97@gmail.com>
2020-10-07 13:11:32 +0530

    function hello added

---
+++ react-tut/prac1/src/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function hello(){
+    return(
+<div>
+<h2>plz work</h2>
+  </div>  );
+}
+export default hello;
--- react-tut/prac1/src/index.js
+++ react-tut/prac1/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const name='sonali';
+import hello from "./hello";
+/*const name='sonali';
 const ele=<h1>hello {name}</h1>;
 ReactDOM.render(
   ele,
   document.getElementById('root')
+);*/
+//ReactDOM.render(<h3>will work ?</h3>,document.getElementById('will'));
+
+
+ReactDOM.render(
+  <hello />,document.getElementById("root")
 );
-ReactDOM.render(<h3>will work ?</h3>,document.getElementById('will'));
