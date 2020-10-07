f714567a5d3f4941e67faad080d5d4c589b60e09
sonallilive <sonallilive97@gmail.com>
2020-10-07 22:07:29 +0530

    bell button created

--- react-tut/prac1/src/hello.js
+++ react-tut/prac1/src/hello.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+// function Hello(){
+//     return(
+// <div>
+// <h2>plz work</h2>
+//   </div>  );
+// }
+
 function Hello(){
-    return(
+const reaction=()=>{
+console.log('ding dong');
+
+};
+return(
 <div>
-<h2>plz work</h2>
-  </div>  );
+    <h1>hello</h1>
+    <button onClick={reaction}>press the belll icon</button>
+</div>
+);
+
 }
+
 export default Hello;
