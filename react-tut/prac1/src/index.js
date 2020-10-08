5067012b1bd6d53ca0b986374b8ca6ceb16b32db
sonallilive <sonallilive97@gmail.com>
2020-10-08 12:18:36 +0530

    time function used

--- react-tut/prac1/src/index.js
+++ react-tut/prac1/src/index.js
@@ -8,7 +8,17 @@ ReactDOM.render(
   document.getElementById('root')
 );*/
 //ReactDOM.render(<h3>will work ?</h3>,document.getElementById('will'));
+function tick() {
+  const element = (
+    <div>
+      <h1>whats the time RN!</h1>
+      <h2>It's {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('clock'));
+}
 
+setInterval(tick, 1000);
 
 ReactDOM.render(
   <Hello />,document.getElementById("root")
